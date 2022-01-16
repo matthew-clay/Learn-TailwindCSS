47973aa3331f3dc0e602c9c5f76e715411406ac5
matthew-clay <alexandermike1999@gmail.com>
2022-01-16 18:13:14 +0630

    Tailwind config & default config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,16 @@
-export const content = ["./src/**/*.{html,js}"];
-export const theme = {
-  extend: {},
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FF6363",
+        secondary: {
+          100: "#E2E2D5",
+          200: "#888883",
+        },
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
 };
-export const plugins = [];
